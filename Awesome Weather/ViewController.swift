adf16aed5e7661d7d2bede4f3c620924f22485d7
Sanam Suri <sanam@socialpeddler.com>
2018-02-11 14:20:10 +0530

    Initial Commit

---
+++ Awesome Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Awesome Weather
+//
+//  Created by Sanam Suri on 11/02/18.
+//  Copyright © 2018 SocialPeddler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
